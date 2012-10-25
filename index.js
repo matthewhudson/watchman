2df6e04c71e8d11f4acc0d1766b0d984046e7b6e
Matthew Hudson <matt@matthewghudson.com>
2012-10-25 15:09:30 -0400

    Basic validation of settings file post-parsing so later code doesn't have to check and re-check in every place that requires certain settings to be present and in a certain format.

--- index.js
+++ index.js
@@ -8,14 +8,59 @@ var http = require('http'),
 	isAlertActive = false;
 
 // Handle uncaughtException
-process.on('uncaughtException', function(err){
-    console.log('Error: %s', err.message);
-	console.log('Exiting...');
-    process.exit(1);
+process.on('uncaughtException', function (err) {
+	exit('Error: ' + err.message);
 });
+
+var exit = function (message) {
+	if (message) {
+		console.log(message);
+	}
+	console.log('Exiting...');
+	process.exit(1);
+};
 
 // Open the config file and establish user settings.
 var config = yaml.eval(fs.readFileSync('config.yaml', 'utf8').toString());
+
+// Validate the config file and ensure mandatory fields exist
+if (!_.has(config, 'interval') || !_.isNumber(config.interval)) {
+	exit('Invalid config. Interval must be a number.');
+}
+
+if (!_.has(config, 'status-codes') || !_.isArray(config['status-codes'])) {
+	exit('Invalid config. Status Codes must be a list.');
+}
+
+if (!_.has(config, 'from') || !_.isString(config.from)) {
+	exit('Invalid config. From must be an email address.');
+}
+
+if (!_.has(config, 'ses') || !_.isObject(config.ses)) {
+	exit('Invalid config. AWS SES settings are missing.');
+}
+
+if (!_.has(config.ses, 'access') || !_.isString(config.ses.access)) {
+	exit('Invalid config. AWS SES Access Key is missing.');
+}
+
+if (!_.has(config.ses, 'secret') || !_.isString(config.ses.access)) {
+	exit('Invalid config. AWS SES Secret Key is missing.');
+}
+
+if (!_.has(config, 'websites') || !_.isObject(config.websites)) {
+	exit('Invalid config. Websites must be an Object.');
+}
+
+// Ensure each website has both a host + contact
+_.each(config.websites, function (website) {
+	if (!_.has(website, 'host') || !_.isString(website.host)) {
+		exit('Invalid config. Each website must have a host.');
+	}
+	if (!_.has(website, 'contact') || !_.isString(website.contact)) {
+		exit('Invalid config. Each website must have a contact email.');
+	}
+});
 
 // Initialize HTTP GET request settings.
 var requestOptions = {
